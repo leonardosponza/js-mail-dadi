bd262e1d3a5ce6914b8b3dd2d29fec1bf13286f1
leonardo <sponzal4@gmail.com>
2021-05-28 16:08:35 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
-var max = 6;
-var min = 1;
-var Computer = Math.floor(Math.random() * (max - min + 1) + min);
+
+var Computer = Math.floor(Math.random()*6) +1 ;
 document.getElementById('pc').innerHTML += Computer;
 
-var Utente = Math.floor(Math.random() * (max - min + 1) + min);
+var Utente = Math.floor(Math.random()*6) +1;
 document.getElementById('user').innerHTML += Utente;
 
 if (Computer > Utente) {
     risultato = 'Computer vince!';
-    document.getElementById('pc').innerHTML = risultato;
 
 } else if (Utente > Computer) {
     risultato = 'Hai vinto!'
-    document.getElementById('user').innerHTML = risultato;
-
+}
+else{
+    risultato = 'Nessun vincitore';
 }
 
-var risultato = 'Nessun vincitore';
 document.getElementById('risultato').innerHTML = risultato;
+
+/* Uguale a 
+
+if (Computer > Utente) {
+    risultato = 'Computer vince!';
+    document.getElementById('risultato').innerHTML = risultato;
+
+} else if (Utente > Computer) {
+    risultato = 'Hai vinto!'
+    document.getElementById('risultato').innerHTML = risultato;
+}
+else{
+    risultato = 'Nessun vincitore';
+    document.getElementById('risultato').innerHTML = risultato;
+}
+
+
+*/
+
+
